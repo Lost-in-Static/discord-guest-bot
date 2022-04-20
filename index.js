05d1abcedae099da43a570a777493329990a79b9
Lost-in-Static <32521625+Lost-in-Static@users.noreply.github.com>
2022-04-20 17:57:33 +0100

    Initial implementation

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require("node:fs");
+const { Client, Collection, Intents, Interaction } = require("discord.js");
+const { token } = require("./config.json");
+
+console.log("Booting the Bot");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isCommand()) return;
+  
+  const command = client.commands.get(interaction.commandName);
+
+  if (command === undefined) return;
+
+  try {
+      await command.execute(interaction);
+  } catch (error) {
+      console.error(error);   
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}  
+});
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.login(token);
