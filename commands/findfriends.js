d13c396e1dc414d6329776779295fa5bcbb44c38
Lost-in-Static <32521625+Lost-in-Static@users.noreply.github.com>
2022-06-10 15:33:57 +0100

    FindFriends now ignores user that ran command

--- commands/findfriends.js
+++ commands/findfriends.js
@@ -6,36 +6,45 @@ module.exports = {
         .setDescription('Shows members in all mutual Voice Channels'),
     execute: async (interaction) => {
         const guilds = interaction.client.guilds.cache
+        const selfID = interaction.user.id
 
         const mutualGuilds = (
             await Promise.all(
                 guilds.map(async (guild) => {
                     try {
-                        await guild.members.fetch(interaction.user.id)
+                        await guild.members.fetch(selfID)
 
-                        const voiceChannels = guild.channels.cache.filter(
-                            (channel) =>
-                                channel.isVoice() && channel.members.size > 0
-                        )
+                        const voiceChannels = guild.channels.cache
+                            .map((channel) => ({
+                                channel,
+                                members: channel.members.filter(
+                                    (member) => member.user.id !== selfID
+                                ),
+                            }))
+                            .filter(
+                                ({ channel, members }) =>
+                                    channel.isVoice() && members.size > 0
+                            )
 
                         return {
                             name: guild.name,
                             voiceChannels,
                         }
-                    } catch (e) { }
+                    } catch (e) {}
                 })
             )
-        ).filter((guild) => guild !== undefined && guild.voiceChannels.size > 0)
+        ).filter(
+            (guild) => guild !== undefined && guild.voiceChannels.length > 0
+        )
 
         const reply = mutualGuilds
             .map((guild) => {
                 const channelsMessage = guild.voiceChannels
-                    .map((channel) => {
-                        const membersMessage = channel.members
+                    .map(({ channel, members }) => {
+                        const membersMessage = members
                             .toJSON()
                             .map((member) => ` - ${member}`)
                             .join('\n')
-
                         return `${channel}\n${membersMessage}\n`
                     })
                     .join('\n')
